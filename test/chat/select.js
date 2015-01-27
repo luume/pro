de7fb0717eae3a97932464ace4f0c0a5df4a5201
luume <darkones@naver.com>
2015-01-27 13:22:01 +0900

    success

---
+++ test/chat/select.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+var util = require('../../afeel/util/vo');
+
+// 자기가한 질문으로 소개 받기
+router.post('/:memberNo', function(req, res) {
+  var memberNo = req.params.memberNo;
+  if(memberNo == "" || memberNo == undefined){
+    res.json({success:0, message:"Error(빈값이 넘어왔습니다.[memberNo])", result:null});
+    return;
+  }
+
+  res.json(util.successCode(res, 'success'));
+
+});
+
+module.exports = router;
