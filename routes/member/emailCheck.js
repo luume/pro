fa4064c2784260c1c8ac8bf00cd3cb9c9ab86302
luume <darkones@naver.com>
2015-01-29 14:44:28 +0900

    email check

---
+++ routes/member/emailCheck.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+var util = require('../../afeel/util/vo');
+
+// 이메일 중복 확인
+router.post('/:accesstoken', function(req, res) {
+
+  res.json(util.successCode(res, 'success'));
+
+});
+
+module.exports = router;
