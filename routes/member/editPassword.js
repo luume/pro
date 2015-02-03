a90a2b1956fadb3d36f928ddafe85f63f1f36078
dragojin <dragojin@naver.com>
2015-02-03 13:22:01 +0900

    ss
    sss

--- routes/member/editPassword.js
+++ routes/member/editPassword.js
@@ -2,15 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 var util = require('../../afeel/util/vo');
+var afeelQuery = require('../../afeel/util/afeelQuery');
 
 router.get('/', function(req, res){
+    var memberNo = req.session.memberNo;
+    var datas = [];
+    datas.push(memberNo);
+
+    global.queryName = 'member';
+    var queryidname = 'editPasswordList';
+
+    afeelQuery.afeelQuery(datas, queryidname , function (err, datas) {
+        if(err){
+            res.json(err);
+        }
+        res.json(util.successCode(res, datas));
+    });
+
 
     var m = util.createValueObject('Member');
 
-    res.json(util.successCode(res, {
-        memberNo : m.Member().memberNo,
-        memberPw : m.Member().memberPw
-    }));
+    //res.json(util.successCode(res, {
+    //    memberNo : m.Member().memberNo,
+    //    memberPw : m.Member().memberPw
+    //}));
 });
 
 router.post('/', function(req, res){
