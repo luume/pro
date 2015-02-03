81daf7197755038e833f6e867de357170fb1088e
luume <darkones@naver.com>
2015-02-03 16:37:55 +0900

    d
    d

--- routes/member/signUp.js
+++ routes/member/signUp.js
@@ -5,7 +5,7 @@ var util = require('../../afeel/util/vo');
 var afeelQuery = require('../../afeel/util/afeelQuery');
 
 router.get('/', function (req ,res) {
-  res.send('<form action="https://54.92.4.84/member/signup" method="post"><input type="file" name="f1"/><br/><input type="file" name="f2"/><br/><input type="file" name="f3"/><br/></form>')
+  res.send('<form action="https://54.92.4.84/member/signup" method="post"><input type="file" name="f1"/><br/><input type="file" name="f2"/><br/><input type="file" name="f3"/><br/><input type="submit" value="ok"/></form>')
 })
 
 // 회원가입
