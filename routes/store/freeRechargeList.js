36f46c58486d28410d01643a6841774aa4e1571b
dragojin <dragojin@naver.com>
2015-02-03 11:12:15 +0900

    myFreeRechargeList
    myFreeRechargeList

--- routes/store/freeRechargeList.js
+++ routes/store/freeRechargeList.js
@@ -4,6 +4,19 @@ var router = express.Router();
 var util = require('../../afeel/util/vo');
 
 router.get('/', function(req, res){
+    var memberNo = req.session.memberNo;
+    var datas = [];
+    datas.push(memberNo);
+
+    global.queryName = 'mystore';
+    var queryidname = 'myFreeRechargeList';
+
+    afeelQuery.afeelQuery(datas, queryidname , function (err, datas) {
+        if(err){
+            res.json(err);
+        }
+        res.json(util.successCode(res, datas));
+    });
 
     var m = util.createValueObject('Member');
 
