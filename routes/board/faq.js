31d6d70d346bf56119b1d72837a08329bc52127e
dragojin <dragojin@naver.com>
2015-02-03 13:34:20 +0900

    dddd
    ddd

--- routes/board/faq.js
+++ routes/board/faq.js
@@ -2,16 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 var util = require('../../afeel/util/vo');
+var afeelQuery = require('../../afeel/util/afeelQuery');
 
 router.get('/', function(req, res){
+    var memberNo = req.session.memberNo;
+    var datas = [];
+    datas.push(memberNo);
+
+    global.queryName = 'board';
+    var queryidname = 'faqList';
+
+    afeelQuery.afeelQuery(datas, queryidname , function (err, datas) {
+        if(err){
+            res.json(err);
+        }
+        res.json(util.successCode(res, datas));
+    });
 
     var faq = util.createValueObject('Faq');
 
-    res.json(util.successCode(res, {
-        faqNo : faq.Faq().faqNo,
-        faqQuestion : faq.Faq().faqQuestion,
-        faqAnswer : faq.Faq().faqAnswer
-    }));
+    //res.json(util.successCode(res, {
+    //    faqNo : faq.Faq().faqNo,
+    //    faqQuestion : faq.Faq().faqQuestion,
+    //    faqAnswer : faq.Faq().faqAnswer
+    //}));
 });
 
 module.exports = router;
