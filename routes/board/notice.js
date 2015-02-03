53d0baa5f93279b13bdabf6e9eabf68d062680f0
dragojin <dragojin@naver.com>
2015-02-03 13:40:18 +0900

    board/notice
    board/notice

--- routes/board/notice.js
+++ routes/board/notice.js
@@ -2,17 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 var util = require('../../afeel/util/vo');
+var afeelQuery = require('../../afeel/util/afeelQuery');
 
 router.get('/', function(req, res){
+    var memberNo = req.session.memberNo;
+    var datas = [];
+    datas.push(memberNo);
+
+    global.queryName = 'board';
+    var queryidname = 'noticeList';
+
+    afeelQuery.afeelQuery(datas, queryidname , function (err, datas) {
+        if(err){
+            res.json(err);
+        }
+        res.json(util.successCode(res, datas));
+    });
 
     var not = util.createValueObject('Notice');
 
-    res.json(util.successCode(res, {
-        noticeNo : not.Notice().noticeNo,
-        noticeTitle : not.Notice().noticeTitle,
-        noticeContent : not.Notice().noticeContent,
-        noticeRegDate : not.Notice().noticeRegDate
-    }));
+    //res.json(util.successCode(res, {
+    //    noticeNo : not.Notice().noticeNo,
+    //    noticeTitle : not.Notice().noticeTitle,
+    //    noticeContent : not.Notice().noticeContent,
+    //    noticeRegDate : not.Notice().noticeRegDate
+    //}));
 });
 
 module.exports = router;
