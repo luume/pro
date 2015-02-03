17dd5583e4a529a8d3dcdda0cfb984af3f5ca223
luume <darkones@naver.com>
2015-02-03 16:19:17 +0900

    d
    d

--- routes/member/signUp.js
+++ routes/member/signUp.js
@@ -14,6 +14,8 @@ router.post('/', function(req, res) {
     return;
   }
 
+
+  console.log('하하호호 ' , req.files);
   //var cir_name = req.body.cir_name;
   var memberEmail = req.body.memberEmail;
   var memberName = req.body.memberName;
@@ -53,70 +55,11 @@ router.post('/', function(req, res) {
     if(err){
       res.json(err);
     }
+
+
+
     res.json(datas);
   });
-  /*CREATE TABLE MEMBER (
-    memberNo        INT     PRIMARY KEY  AUTO_INCREMENT,
-    memberEmail     VARCHAR(20) NOT NULL,
-    memberNick      VARCHAR(20) NOT NULL,
-    memberTel       INT         NULL,
-    memberAdd       VARCHAR(50) NOT NULL,
-    memberName      VARCHAR(30) NOT NULL,
-    memberGender    VARCHAR(1)  NOT NULL,
-    memberBirth     VARCHAR(8)  NOT NULL,
-    memberHeight    INT     NOT NULL,                                      1
-    memberPw        VARCHAR(20) NOT NULL,
-    memberHobby     VARCHAR(20) NOT NULL,
-    memberJob       VARCHAR(20) NOT NULL,
-    memberLevel     INT     NOT NULL DEFAULT 1,
-    memberSmoking   VARCHAR(1)  NOT NULL,
-    memberMatchCnt  INT     NOT NULL DEFAULT 0,
-    memberHPYn      INT     NULL,
-    memberEmailYn   INT     NOT NULL DEFAULT 0,
-    memberSNSYn     INT     NULL,
-    memberStMeeting DATETIME not NULL     DEFAULT now(),
-    memberPushYn    INT     NOT NULL DEFAULT 0,
-    memberPenalty   INT     NULL     DEFAULT 0,
-    memberCash      INT     NOT NULL DEFAULT 0,
-    memberBestQ     INT     NULL,
-    memberWithdraw  INT     NULL     DEFAULT 0,
-    memberWithdrawReason VARCHAR(5000) NULL,
-    memberToken	VARCHAR(5000) 	   NULL
-  );
-*/
-/*
-  Member      memberEmail: 이메일
-  Member      memberName: 실명
-  Member      memberNick: 닉네임
-  Member      memberPw: 비밀번호
-  Member      memberGender: 성별
-  Member      memberBirth: 생년월일
-  Member      memberHobby: 취미/특기
-  Member      memberHeight : 키
-  Member      memberAdd : 거주지
-  Member      memberJob : 직업
-  Profil           profilOriginalFileName: 프로필 사진 파일명*/
-
-  /*var m = util.createValueObject('Member');
-
-  var p = util.createValueObject('Profil');
-
-  res.json(util.successCode(res,
-    {
-      memberEmail : m.Member().memberEmail,
-      memberName : m.Member().memberName,
-      memberNick : m.Member().memberNick,
-      memberPw : m.Member().memberPw,
-      memberTel : m.Member().memberTel,
-      memberGender : m.Member().memberGender,
-      memberBirth : m.Member().memberBirth,
-      memberHobby : m.Member().memberHobby,
-      memberHeight  : m.Member().memberHeight ,
-      memberAdd   : m.Member().memberAdd  ,
-      memberJob    : m.Member().memberJob   ,
-      profilOriginalFileName    : p.Member().profilOriginalFileName
-    }
-  ));*/
 
 });
 
