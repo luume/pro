538ddb611ac72563ae8523cb8c1103589d29c8ba
luume <darkones@naver.com>
2015-02-03 13:09:38 +0900

    dd

--- routes/expeople/endMatchList.js
+++ routes/expeople/endMatchList.js
@@ -2,19 +2,38 @@ var express = require('express');
 var router = express.Router();
 
 var util = require('../../afeel/util/vo');
+var afeelQuery = require('../../afeel/util/afeelQuery');
 
 router.get('/', function(req, res){
     //console.log('sdfsdfsdfsdfsdf');
-    var m = util.createValueObject('Member');
+  /*  var m = util.createValueObject('Member');
     var cr = util.createValueObject('ChatRoom');
     var f = util.createValueObject('Feeling');
     var ft = util.createValueObject('Feeling_Type');
     var p = util.createValueObject('Profil');
+*/
 
 
+    var memberEmail = req.body.memberEmail;
+
+    var datas = [];
+    datas.push(req.session.memberNo);
+    datas.push(req.session.memberNo);
+
+    //datas.push(profilOriginalFileName);
+
+    global.queryName = 'expeople';
+    var queryidname = 'endMatchList';
+    console.log('datas',datas);
+    afeelQuery.afeelQuery(datas, queryidname , function (err, datas) {
+        if(err){
+            res.json(err);
+        }
+        res.json(datas);
+    });
   /*  profilSaveFileName : [ '../../images/Hydrangeas-thumbnail.jpg' , '../../images/Jellyfish-thumbnaill.jpg', '../../images/Koala-thumbnaill.jpg',
         '../../images/Penguins-thumbnail.jpg', '../../images/Lighthouse-thumbnail.jpg', '../../images/Tulips-thumbnail.jpg']*/
-    res.json({
+   /* res.json({
           success : 1,
           message : 'OK',
           result : [{
@@ -99,14 +118,14 @@ router.get('/', function(req, res){
                   chatroomRegdate : m.ChatRoom().chatroomRegdate,
                   fTypeArray : ['섹시한', '육감적인', '스마트한'],
                   feelingRate : f.Feeling().feelingRate,
-                  profilSaveFileName : 'https://54.92.4.84/images/Tulips-thumbnail.jpg'
+                  profilSaveFileName : 'https://54.92.4.84/images/Lighthouse-thumbnail.jpg'
               }
 
           ]
 
       }
 
-    );
+    );*/
 
 
 });
