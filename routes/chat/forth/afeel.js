02eb25b4a9e0a91a2fe5d7ec3f605d96659733d9
dragojin <dragojin@naver.com>
2015-02-03 14:47:05 +0900

    ee
    ee

--- routes/chat/forth/afeel.js
+++ routes/chat/forth/afeel.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var util = require('../../../afeel/util/vo');
+var afeelQuery = require('../../../afeel/util/afeelQuery');
 
 router.post('/', function(req, res){
 
@@ -10,15 +11,33 @@ router.post('/', function(req, res){
         res.json({success:0, message:"Error(빈값이 넘어왔습니다.[chatroomNo])", result:null});
         return;
     }
+    var memberNo = req.session.memberNo;
+    if(memberNo == "" || memberNo == undefined){
+        res.json({success:0, message:"Error(빈값이 넘어왔습니다.[memberNo])", result:null});
+        return;
+    }
+
+    var datas = [];
+    datas.push(memberNo);
+
+    global.queryName = 'mystore';
+    var queryidname = 'myMainList';
+
+    afeelQuery.afeelQuery(datas, queryidname , function (err, datas) {
+        if(err){
+            res.json(err);
+        }
+        res.json(util.successCode(res, datas));
+    });
 
     var cr = util.createValueObject('ChatRoom');
     var m = util.createValueObject('Member');
     var c = util.createValueObject('Cash');
 
-    res.json(util.successCode(res, {
-        chatroomNo : cr.ChatRoom().chatroomNo,
-        cashAmount : c.Cash().cashAmount
-    }));
+    //res.json(util.successCode(res, {
+    //    chatroomNo : cr.ChatRoom().chatroomNo,
+    //    cashAmount : c.Cash().cashAmount
+    //}));
 
 });
 
