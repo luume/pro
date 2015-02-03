1b5121014003e8cedf2048160b380e9191f6d0c3
dragojin <dragojin@naver.com>
2015-02-03 13:47:10 +0900

    dd
    dd

--- routes/profil/main.js
+++ routes/profil/main.js
@@ -2,26 +2,39 @@ var express = require('express');
 var router = express.Router();
 
 var util = require('../../afeel/util/vo');
+var afeelQuery = require('../../afeel/util/afeelQuery');
 
 router.get('/', function(req, res){
+    var memberNo = req.session.memberNo;
+    var datas = [];
+    datas.push(memberNo);
 
+    global.queryName = 'profil';
+    var queryidname = 'profilList';
+
+    afeelQuery.afeelQuery(datas, queryidname , function (err, datas) {
+        if(err){
+            res.json(err);
+        }
+        res.json(util.successCode(res, datas));
+    });
 
     var m = util.createValueObject('Member');
     var pro = util.createValueObject('Profil');
 
-    res.json( util.successCode(res, {
-        memberName : m.Member().memberName,
-        memberBirth : m.Member().memberBirth,
-        memberHobby : m.Member().memberHobby,
-        memberAdd : m.Member().memberAdd,
-        memberJob : m.Member().memberJob,
-        memberHeight : m.Member().memberHeight,
-        profilOriginalFileName  : ['https://54.92.4.84/images/Hydrangeas-thumbnail.jpg',
-            'https://54.92.4.84/images/Jellyfish-thumbnail.jpg',
-            'https://54.92.4.84/images/Koala-thumbnail.jpg',
-            'https://54.92.4.84/images/Penguins-thumbnail.jpg',
-            'https://54.92.4.84/images/Lighthouse-thumbnail.jpg']
-    }));
+    //res.json( util.successCode(res, {
+    //    memberName : m.Member().memberName,
+    //    memberBirth : m.Member().memberBirth,
+    //    memberHobby : m.Member().memberHobby,
+    //    memberAdd : m.Member().memberAdd,
+    //    memberJob : m.Member().memberJob,
+    //    memberHeight : m.Member().memberHeight,
+    //    profilOriginalFileName  : ['https://54.92.4.84/images/Hydrangeas-thumbnail.jpg',
+    //        'https://54.92.4.84/images/Jellyfish-thumbnail.jpg',
+    //        'https://54.92.4.84/images/Koala-thumbnail.jpg',
+    //        'https://54.92.4.84/images/Penguins-thumbnail.jpg',
+    //        'https://54.92.4.84/images/Lighthouse-thumbnail.jpg']
+    //}));
 });
 
 module.exports = router;
