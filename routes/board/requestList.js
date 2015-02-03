c5d5f49927a5df60fba5cd5b80c541221bc3fc3b
dragojin <dragojin@naver.com>
2015-02-03 13:27:26 +0900

    ee
    ee

--- routes/board/requestList.js
+++ routes/board/requestList.js
@@ -2,17 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 var util = require('../../afeel/util/vo');
+var afeelQuery = require('../../afeel/util/afeelQuery');
 
 router.get('/', function(req, res){
+    var memberNo = req.session.memberNo;
+    var datas = [];
+    datas.push(memberNo);
+
+    global.queryName = 'board';
+    var queryidname = 'requestList';
+
+    afeelQuery.afeelQuery(datas, queryidname , function (err, datas) {
+        if(err){
+            res.json(err);
+        }
+        res.json(util.successCode(res, datas));
+    });
 
     var re = util.createValueObject('Request');
 
-    res.json(util.successCode(res, {
-        requestNo : re.Request().requestNo,
-        requestTitle : re.Request().requestTitle,
-        requestContent : re.Request().requestContent,
-        requestRegDate : re.Request().requestRegDate
-    }));
+    //res.json(util.successCode(res, {
+    //    requestNo : re.Request().requestNo,
+    //    requestTitle : re.Request().requestTitle,
+    //    requestContent : re.Request().requestContent,
+    //    requestRegDate : re.Request().requestRegDate
+    //}));
 });
 
 module.exports = router;
