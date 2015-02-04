8bd0e1a485dafba6c8778c039165d8d0553c346d
luume <darkones@naver.com>
2015-02-04 20:28:23 +0900

    d
    d

--- routes/profil/main.js
+++ routes/profil/main.js
@@ -38,7 +38,7 @@ router.get('/', function(req, res){
             }
         });
 
-        datas[profilOriginalFileName] = profilOriginalFileName;
+        datas['ssss'] = profilOriginalFileName;
 
 
         console.log('리얼 datas = ' , datas);
