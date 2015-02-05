8438d82e97915e1c51e08fc51ecc9bfea1b3a84f
dragojin <dragojin@naver.com>
2015-02-05 19:47:09 +0900

    ddd
    dd

--- routes/expeople/privateChatList/main.js
+++ routes/expeople/privateChatList/main.js
@@ -14,38 +14,40 @@ router.get('/', function(req, res){
     var datas = [];
     datas.push(memberNo);
 
-    global.queryName = 'member';
-    var queryidname = 'genderMember';
+    //global.queryName = 'member';
+    //var queryidname = 'genderMember';
+
+    //afeelQuery.afeelQuery(datas, queryidname , function (err, datas) {
+    //    if(err){
+    //        res.json(err);
+    //    }
+    //    if (datas[0].memberGender == 'M') { //datas[0].memberGender 로 현재 사용자의 성별을 파악함
+    //        console.log('남자다');
+    //        var datas = [];
+    //        //datas.push(memberNo);
+    //
+    //    } else {
+    //        console.log('여자다');
+    //
+    //
+    //    }
+    //
+    //
+    //
+    //});
+
+    var datas = [];
+    datas.push(memberNo);
+
+    global.queryName = 'expeople';
+    var queryidname = 'myPrivateChatList_W';
 
     afeelQuery.afeelQuery(datas, queryidname , function (err, datas) {
         if(err){
             res.json(err);
         }
-        if (datas[0].memberGender == 'M') { //datas[0].memberGender 로 현재 사용자의 성별을 파악함
-            console.log('남자다');
-            var datas = [];
-            //datas.push(memberNo);
-
-        } else {
-            console.log('여자다');
-
-
-        }
-        var datas = [];
-        //datas.push(memberNo);
-
-        global.queryName = 'expeople';
-        var queryidname = 'myPrivateChatList_W';
-
-        afeelQuery.afeelQuery(datas, queryidname , function (err, datas) {
-            if(err){
-                res.json(err);
-            }
-            //console.log('data',datas);
-            res.json(util.successCode(res, datas));
-        });
-
-
+        //console.log('data',datas);
+        res.json(util.successCode(res, datas));
     });
 
 
