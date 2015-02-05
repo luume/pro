751b46d57c9200093fba97a765c882383fa374dc
luume <darkones@naver.com>
2015-02-05 16:45:40 +0900

    d
    d

--- routes/member/signUp.js
+++ routes/member/signUp.js
@@ -18,7 +18,7 @@ router.post('/', function(req, res) {
   //   return;
   // }
 
-  res.send('<script>파일객체:' + req.files + '</script>');
+  res.send('<script>alert("파일객체:" + req.files );' + '</script>');
 
   console.log('bodyssssssss = ', req.body);
   console.log('파일 객체ss', req.files);
