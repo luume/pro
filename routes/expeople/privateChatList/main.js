a02371e687135a3e32aa02b4d285bda96ecfb09b
dragojin <dragojin@naver.com>
2015-02-05 17:52:23 +0900

    dd
    dd

--- routes/expeople/privateChatList/main.js
+++ routes/expeople/privateChatList/main.js
@@ -44,51 +44,51 @@ router.get('/', function(req, res){
     var pm = util.createValueObject('Private_Message');
     var p = util.createValueObject('Profil');
 
-    res.json({
-            success : 1,
-            message : 'OK',
-            result : [{
-                memberName :  m.Member().memberName,
-                messageData : pm.Private_Message().messageData,
-                messageDate : pm.Private_Message().messageDate,
-                profilSaveFileName : 'https://54.92.4.84/images/Hydrangeas-thumbnail.jpg'
-            },
-                {
-                    memberName :  m.Member().memberName,
-                    messageData : pm.Private_Message().messageData,
-                    messageDate : pm.Private_Message().messageDate,
-                    profilSaveFileName : 'https://54.92.4.84/images/Jellyfish-thumbnail.jpg'
-                },
-                {
-                    memberName :  m.Member().memberName,
-                    messageData : pm.Private_Message().messageData,
-                    messageDate : pm.Private_Message().messageDate,
-                    profilSaveFileName : 'https://54.92.4.84/images/Koala-thumbnail.jpg'
-                },
-                {
-                    memberName :  m.Member().memberName,
-                    messageData : pm.Private_Message().messageData,
-                    messageDate : pm.Private_Message().messageDate,
-                    profilSaveFileName : 'https://54.92.4.84/images/Penguins-thumbnail.jpg'
-                },
-                {
-                    memberName :  m.Member().memberName,
-                    messageData : pm.Private_Message().messageData,
-                    messageDate : pm.Private_Message().messageDate,
-                    profilSaveFileName : 'https://54.92.4.84/images/Lighthouse-thumbnail.jpg'
-                },
-                {
-                    memberName :  m.Member().memberName,
-                    messageData : pm.Private_Message().messageData,
-                    messageDate : pm.Private_Message().messageDate,
-                    profilSaveFileName : 'https://54.92.4.84/images/Tulips-thumbnail.jpg'
-                }
-
-            ]
-
-        }
-
-    );
+    //res.json({
+    //        success : 1,
+    //        message : 'OK',
+    //        result : [{
+    //            memberName :  m.Member().memberName,
+    //            messageData : pm.Private_Message().messageData,
+    //            messageDate : pm.Private_Message().messageDate,
+    //            profilSaveFileName : 'https://54.92.4.84/images/Hydrangeas-thumbnail.jpg'
+    //        },
+    //            {
+    //                memberName :  m.Member().memberName,
+    //                messageData : pm.Private_Message().messageData,
+    //                messageDate : pm.Private_Message().messageDate,
+    //                profilSaveFileName : 'https://54.92.4.84/images/Jellyfish-thumbnail.jpg'
+    //            },
+    //            {
+    //                memberName :  m.Member().memberName,
+    //                messageData : pm.Private_Message().messageData,
+    //                messageDate : pm.Private_Message().messageDate,
+    //                profilSaveFileName : 'https://54.92.4.84/images/Koala-thumbnail.jpg'
+    //            },
+    //            {
+    //                memberName :  m.Member().memberName,
+    //                messageData : pm.Private_Message().messageData,
+    //                messageDate : pm.Private_Message().messageDate,
+    //                profilSaveFileName : 'https://54.92.4.84/images/Penguins-thumbnail.jpg'
+    //            },
+    //            {
+    //                memberName :  m.Member().memberName,
+    //                messageData : pm.Private_Message().messageData,
+    //                messageDate : pm.Private_Message().messageDate,
+    //                profilSaveFileName : 'https://54.92.4.84/images/Lighthouse-thumbnail.jpg'
+    //            },
+    //            {
+    //                memberName :  m.Member().memberName,
+    //                messageData : pm.Private_Message().messageData,
+    //                messageDate : pm.Private_Message().messageDate,
+    //                profilSaveFileName : 'https://54.92.4.84/images/Tulips-thumbnail.jpg'
+    //            }
+    //
+    //        ]
+    //
+    //    }
+    //
+    //);
     //
 
     //
