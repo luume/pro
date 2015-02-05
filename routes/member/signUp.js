b430b45c96fc9a83b21be43fda2254199d3819f3
luume <darkones@naver.com>
2015-02-05 16:48:51 +0900

    d
    d

--- routes/member/signUp.js
+++ routes/member/signUp.js
@@ -18,14 +18,14 @@ router.post('/', function(req, res) {
   //   return;
   // }
 
-  res.json("파일객체:" + req.files);
+
 
   console.log('bodyssssssss = ', req.body);
   console.log('파일 객체ss', req.files);
   console.log('길이1', req.files.length);
   console.log('길이2', req.files.profilOriginalFileName);
   console.log('길이3', req.files.profilOriginalFileName.length);
-
+  res.json("파일객체:" + req.files);
 
   //var cir_name = req.body.cir_name;
   var memberEmail = req.body.memberEmail;
