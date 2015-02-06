fcb09a614ee46a08700032f69b4ee109021b30b2
luume <darkones@naver.com>
2015-02-06 18:33:50 +0900

    ddddddddddddd

--- routes/chat/privateChat/main.js
+++ routes/chat/privateChat/main.js
@@ -24,12 +24,10 @@ router.get('/:privateRoomNo', function(req, res){
     afeelQuery.afeelQuery(datas, queryidname , function (err, datas) {
         if(err){
             res.json(err);
-            global.afeelCon.release();
             return;
         }
         if(datas == false){
-            res.json({ success : 0 , message : '데이터 없음', result : null});
-            global.afeelCon.release();
+            res.json({ success : 0 , message : '데이터 없음', result : null});;
             return;
         }
 
