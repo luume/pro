6037e4a79c8c0e5c615cda2e29552c2d6ccf685b
dragojin <dragojin@naver.com>
2015-02-06 14:57:55 +0900

    ddd
    dd

--- routes/board/request.js
+++ routes/board/request.js
@@ -6,15 +6,15 @@ var util = require('../../afeel/util/vo');
 router.post('/', function(req, res){
 
     var requestTitle = req.params.requestTitle;
-    if(requestTitle == "" || requestTitle == undefined){
-        res.json({success:0, message:"Error(빈값이 넘어왔습니다.[requestTitle])", result:null});
-        return;
-    }
+    //if(requestTitle == "" || requestTitle == undefined){
+    //    res.json({success:0, message:"Error(빈값이 넘어왔습니다.[requestTitle])", result:null});
+    //    return;
+    //}
     var requestContent = req.params.requestContent;
-    if(requestContent == "" || requestContent == undefined){
-        res.json({success:0, message:"Error(빈값이 넘어왔습니다.[requestContent])", result:null});
-        return;
-    }
+    //if(requestContent == "" || requestContent == undefined){
+    //    res.json({success:0, message:"Error(빈값이 넘어왔습니다.[requestContent])", result:null});
+    //    return;
+    //}
 
    res.json(util.successCode(res, 1));
 
