b88df1d24fafe9ae13edc8a1c2a8b3cd59aecb10
luume <darkones@naver.com>
2015-02-06 19:43:39 +0900

    dd

--- afeel/util/afeelQuery.js
+++ afeel/util/afeelQuery.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 var xml_digester = require("xml-digester");
 var digester = xml_digester.XmlDigester({});
 
+var async = require('async');
 
 /**
  * mybatis 같이 xml 파일로 쿼리를 읽어오고 실행하기 위한 모듈
@@ -12,132 +13,109 @@ var digester = xml_digester.XmlDigester({});
  * @param callback (err, row)   err : 에러 일경우를 위한 매개변수. null이면 에러가 발생하지않음.
  *                                       row : 정상처리되었을 경우 쿼리의 결과가 담김
  */
+var query;
 exports.afeelQuery = function(bindQuery , queryId,  callback) {
   //console.log('확인');
   fs.readFile(global.directoryPath + global.queryName + '.xml','utf8', function(error, data) {
     if (error) {
       console.log(error);
     } else {
-      digester.digest(data, function(error, result) {
-        if (error) {
-          console.log(error);
-        } else {
-          var query;
-          var tobj = {};
-          tobj = result.query.myquery;
-          //console.log('xml',result.query.myquery);
-          //console.log('렝스',result.query.myquery.length);
-          var count = 0;
-          var k;
-          for (k in tobj) {
-            if (tobj.hasOwnProperty(k)) {
-              //console.log(tobj.hasOwnProperty(k));
-              count++;
-            }
-          }
-          for(var i = 0 ; i < count; i++){
-            if(result.query.myquery.length == undefined){
-              if(result.query.myquery.id == queryId){
-                query = util.format(result.query.myquery._text);
-                break;
+
+      async.waterfall([
+        function (callback) {
+
+          digester.digest(data, function(error, result) {
+            if (error) {
+              console.log(error);
+            } else {
+
+              var tobj = {};
+              tobj = result.query.myquery;
+              //console.log('xml',result.query.myquery);
+              //console.log('렝스',result.query.myquery.length);
+              var count = 0;
+              var k;
+              for (k in tobj) {
+                if (tobj.hasOwnProperty(k)) {
+                  //console.log(tobj.hasOwnProperty(k));
+                  count++;
+                }
               }
-            }
+              for(var i = 0 ; i < count; i++){
+                if(result.query.myquery.length == undefined){
+                  if(result.query.myquery.id == queryId){
+                    query = util.format(result.query.myquery._text);
+                    break;
+                  }
+                }
 
-            if(result.query.myquery[i].id == queryId){
-              query = util.format(result.query.myquery[i]._text);
-              break;
-            }
-          } // for end
+                if(result.query.myquery[i].id == queryId){
+                  query = util.format(result.query.myquery[i]._text);
+                  break;
+                }
+              } // for end
 
-          global.pool.getConnection(function(err, conn) {
-            //if(global.afeelCon == '' || global.afeelCon == undefined == global.afeelCon == null)
-             // global.afeelCon = conn;
-            
-            if(err) console.error('err 발생 >>>>>', err);
+
+            }
+          });
+          callback(null, query);
+        }
+      ], function (err, result) {
+
+
+
+        global.pool.getConnection(function(err, conn) {
+          if (err) console.error('err 발생 >>>>>', err);
 
 
           //  global.afeelCon.beginTransaction(function(err) {
-             conn.query(query, bindQuery,  function(err, row) {
-               // console.log('쿼리 ' ,util.format(query));
-               // console.log('파라미터', bindQuery);
-              //  console.log('row', row);
-                if(err){
-                  //global.afeelCon.release();
-                  console.log(queryId  + ' =  ' + err);
-                  callback(
-                    {
-                      success: 0,
-                      message:err,
-                      result : null
-                    }
-                  );
+          conn.query(query, bindQuery, function (err, row) {
 
-                  return;
-                };
-
-                if(row.affectedRows == 0 || row == null || row == undefined || row == false){
-                  //global.afeelCon .release();
-                  console.log('0행입니다.');
-                  global.isQuerySuccess = false;
-                  conn.release();
-
-                  callback(
-                    {
-                      success: 0,
-                      message: 'SQL 실행 결과가 존재하지 않습니다.',
-                      result : null
-                    }
-                  );
-                  return;
+            if (err) {
+              //global.afeelCon.release();
+              console.log(queryId + ' =  ' + err);
+              callback(
+                {
+                  success: 0,
+                  message: err,
+                  result : null
                 }
+              );
 
-                //} // if end
-                //  console.log('쿼리결과', row);
-                global.isQuerySuccess = true;
-                conn.release();
-               //conn.release();
-                callback(null, row);
-              }); // 쿼리 문 종료
+              return;
+            }
+            ;
 
-        //    }); // begin end
-  /*          conn.query(query, bindQuery,  function(err, row) {
-              console.log('쿼리 ' ,util.format(query));
-              console.log('파라미터', bindQuery);
-              console.log('row', row);
-              if(err){
-                conn.release();
-                callback(
-                  {
-                    success: 0,
-                    message:err,
-                    result : null
-                  }
-                );
-              };
+            if (row.affectedRows == 0 || row == null || row == undefined || row == false) {
+              //global.afeelCon .release();
+              console.log('0행입니다.');
+              global.isQuerySuccess = false;
+              conn.release();
 
-              if(row.affectedRows == 0 || row == null || row == undefined || row == false){
-               // conn.release();
-                global.isQuerySuccess = false;
-                callback(
-                  {
-                    success: 0,
-                    message: '개행 결과가 존재하지 않습니다.',
-                    result : null
-                  }
-                );
-              }
+              callback(
+                {
+                  success: 0,
+                  message: 'SQL 실행 결과가 존재하지 않습니다.',
+                  result : null
+                }
+              );
+              return;
+            }
 
             //} // if end
             //  console.log('쿼리결과', row);
-              global.isQuerySuccess = true;
-              //conn.release();
-              callback(null, row);
+            global.isQuerySuccess = true;
+            conn.release();
+            //conn.release();
+            callback(null, row);
           }); // 쿼리 문 종료
-*/
-          });
+
+        });
+
 
         }
-      });
+      );
+
 
     }
 
