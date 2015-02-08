c73c2549e459c1ca07cbd835e775322b43b8dcf5
dragojin <dragojin@naver.com>
2015-02-08 15:31:15 +0900

    myquestion/regquestion
    myquestion/regquestion

--- routes/myquestion/regQuestion.js
+++ routes/myquestion/regQuestion.js
@@ -2,15 +2,43 @@ var express = require('express');
 var router = express.Router();
 
 var util = require('../../afeel/util/vo');
+var afeelQuery = require('../../afeel/util/afeelQuery');
 
-// 소개 받기
+// 질문 저장
 router.post('/', function(req, res) {
 
-  var m = util.createValueObject('Member');
-  var f = util.createValueObject('Feeling');
-  var p = util.createValueObject('Profil');
+  var memberNo = req.session.memberNo;
+  var questionData = req.body.questionData;
+  var questionGuide = req.body.questionGuide;
 
-  res.json(util.successCode(res,'success'));
+  var datas = [];
+  datas.push(memberNo);
+  datas.push(questionData);
+  datas.push(questionGuide);
+
+  global.queryName = 'myquestion';
+  var queryidname = 'regMyquestion';
+
+  global.pool.getConnection(function (err, conn) {
+    conn.beginTransaction(function (err) {
+      afeelQuery.afeelQuery(datas, queryidname , function (err, row) {
+        if(err){
+          res.json(err);
+          return;
+        }
+        if(row.affectedRows == 1){
+          res.json(util.successCode(res, '[success]'));
+        }
+        else{
+          res.json(err);
+          return;
+        }
+      });
+    }); // 트랜잭션 종료
+  });
+
+
+  res.json(util.successCode(res,'[success]'));
 });
 
 module.exports = router;
