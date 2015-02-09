7fb5e9afc7d0f9027352e317b9c29d8bf971f6ee
luume <darkones@naver.com>
2015-02-09 13:27:03 +0900

    s
    s

--- routes/member/login.js
+++ routes/member/login.js
@@ -48,4 +48,44 @@ router.post('/', function(req, res){
 
 });
 
+
+router.post('/:memberToken', function(req, res){
+
+    var memberToken = req.params.memberToken;
+
+    var datas = [];
+    datas.push(memberToken);
+
+    var varibCheck = function (test ,num) {
+        if(test.length > num){
+
+        }
+    }
+
+    global.queryName = 'member';
+    var queryidname = 'facebookLoginMember';
+    console.log('datas',datas);
+    afeelQuery.afeelQuery(datas, queryidname , function (err, datas) {
+        if(err){
+            res.json(err);
+            return;
+        }
+
+        if(datas == null || datas == false){
+            res.json({success:0, message:'토큰이 존재하지 않습니다.', result:null});
+            return;
+        }else{
+            req.session.memberNo  = datas[0].memberNo;
+            res.json( { success : 1 , message : 'ok' ,result : datas  } );
+            return;
+        }
+
+        //console.log('세션 정보 = > ', req.session);
+
+
+    });
+
+
+});
+
 module.exports = router;
