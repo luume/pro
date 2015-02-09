1e240f04f3bb63369bed714f7b4e78fe8e4ad369
dragojin <dragojin@naver.com>
2015-02-09 16:08:45 +0900

    ㄴㄴ
    ㄴㄴㄴ

--- routes/home/main.js
+++ routes/home/main.js
@@ -34,8 +34,9 @@ router.get('/', function(req, res) {
     //  temp.aaa = arr;
     //
     //}
+    var arr = [];
     for(i=0; i<datas.length; i++){
-      var arr = [];
+      
       arr.push(datas[i].profilThumbnail);
       //datas[i].profilThumbnail = arr;
       //[i].profilThumbnail = arr;
