aa407407a6c7b6fc05e0033acbab9991b9ac3900
dragojin <dragojin@naver.com>
2015-02-09 14:17:02 +0900

    ㅇㅇ
    ㅇㅇ

--- routes/member/withDraw.js
+++ routes/member/withDraw.js
@@ -11,8 +11,31 @@ router.post('/', function(req, res){
     //    res.json({success:0, message:"Error(빈값이 넘어왔습니다.[memberWithdrawReason])", result:null});
     //    return;
     //}
+    var datas = [];
+    datas.push(memberWithdrawReason);
+    datas.push(memberNo);
 
-    res.json(util.successCode(res, '[success]'));
+    global.queryName = 'member';
+    var queryidname = 'withdrawMember';
+    //회원탈퇴
+    global.pool.getConnection(function (err, conn) {
+        conn.beginTransaction(function (err) {
+            afeelQuery.afeelQuery(datas, queryidname , function (err, row) {
+                if(err){
+                    res.json(err);
+                    return;
+                }
+                if(row.affectedRows == 1){
+                    res.json(util.successCode(res, '[success]'));
+                } else {
+                    res.json({ success : 0 , message : 'fail', result : null});
+                    return;
+                }
+            });
+        }); // 트랜잭션 종료
+    });
+
+
 
 });
 
