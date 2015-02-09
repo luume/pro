638db679f428f099d1ecf2c2d6665b64eaaf7b47
dragojin <dragojin@naver.com>
2015-02-09 10:16:46 +0900

    ㅇㅇ
    ㄴㄴㅇ

--- routes/profil/yourprofil.js
+++ routes/profil/yourprofil.js
@@ -9,52 +9,7 @@ router.get('/', function(req, res){
     var datas = [];
     datas.push(memberNo);
 
-    global.queryName = 'profil';
-    var queryidname = 'profilList';
-
-    afeelQuery.afeelQuery([req.session.memberNo], queryidname , function (err, datas) {
-        if(err){
-            res.json(err);
-            return;
-        }
-        if(datas == false){
-            res.json({ success : 0 , message : '데이터 없음', result : null});
-            return;
-        }
-
-        var profilThumbnail = [];
-        var temp;
-        afeelQuery.afeelQuery([req.session.memberNo], 'profilFileSelect', function (err, profilName) {
-            if(err){res.json(err);}
-
-
-            var arr = [];
-            var a = 0;
-            async.each(profilName, function (row, callback) {
-                arr.push(row.profilThumbnail);
-
-                callback();
-
-            }, function(err){
-                profilThumbnail = arr;
-                temp = datas;
-                datas[0].profilThumbnail = arr;
-                temp.aaa = arr;
-
-                res.json({success:1, message:'ok', result:datas[0]});
-            });
-
-        });
-
-
-
-
-
-
-
-
-        //res.json({success:1, message:'ok', result: temp });
-    });
+    
 
     var m = util.createValueObject('Member');
     var pro = util.createValueObject('Profil');
