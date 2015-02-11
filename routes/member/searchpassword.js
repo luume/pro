12d552f85ec9f85815f82c7a7c25023956ccbbee
dragojin <dragojin@naver.com>
2015-02-11 20:03:39 +0900

    ddd
    dd

--- routes/member/searchpassword.js
+++ routes/member/searchpassword.js
@@ -17,12 +17,15 @@ router.post('/', function(req, res) {
     });
     var password = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     var tempPwd = 0;
+    var pwd = '';
 
     for(i=0; i<4; i++){
         var tempPwd = password[Math.ceil(Math.random()*35)]
-        console.log(tempPwd);
+
+        pwd += tempPwd
     }
 
+    console.log('pwd',pwd);
     //console.log('pp',password);
     server.send({
         text : 'i hope eat something',
