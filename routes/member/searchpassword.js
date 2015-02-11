397f4c7289ba10a6d96e8c54cdffaf13451a2e79
dragojin <dragojin@naver.com>
2015-02-11 19:20:40 +0900

    dd
    dd

--- routes/member/searchpassword.js
+++ routes/member/searchpassword.js
@@ -6,6 +6,28 @@ var util = require('../../afeel/util/vo');
 // 이메일 인증
 router.post('/', function(req, res) {
 
+    var memberEmail = req.body.memberEmail;
+
+    var server = memberEmail.server.connect({
+        user: 'khj98291@gmail.com',
+        password: 'fdfd04814',   //내 비번!!!
+        host: 'smtp.gmail.com',
+        ssl: true
+    });
+
+    server.send({
+        text : 'i hope eat something',
+        from : 'you <khj98291@gmail.com>',
+        to : 'lee <dragojin@naver.com>, lcb <khj98291@gmail.com>',
+        //cc : 'ccl <hacit@naver.com>',
+        subject : 'text email',
+        attachment:
+            [
+                {data:"<html>i<i>hope</i> eat something! check</html>",
+                    alternative:true}
+            ]
+    }, function(err, message){console.log(err || message); });
+    res.json(util.successCode(res, 'success'));
 
 
 });
