92083de277bcebcbc6b10d794a30db25a0e4a484
dragojin <dragojin@naver.com>
2015-02-12 14:54:02 +0900

    dd
    ddd

--- routes/chat/privateChat/sendChat.js
+++ routes/chat/privateChat/sendChat.js
@@ -6,17 +6,19 @@ var util = require('../../../afeel/util/vo');
 router.post('/', function(req, res){
 
     var privateRoomNo = req.body.privateRoomNo;
-    if(privateRoomNo == "" || privateRoomNo == undefined){
-        res.json({success:0, message:"Error(빈값이 넘어왔습니다.[privateRoomNo])", result:null});
-        return;
-    }
+    //if(privateRoomNo == "" || privateRoomNo == undefined){
+    //    res.json({success:0, message:"Error(빈값이 넘어왔습니다.[privateRoomNo])", result:null});
+    //    return;
+    //}
     var messageData = req.body.messageData;
-    if(messageData == "" || messageData == undefined){
-        res.json({success:0, message:"Error(빈값이 넘어왔습니다.[messageData])", result:null});
-        return;
-    }
+    //if(messageData == "" || messageData == undefined){
+    //    res.json({success:0, message:"Error(빈값이 넘어왔습니다.[messageData])", result:null});
+    //    return;
+    //}
+    var d = new Date();
+    var tempDate = d.getFullYear() + (d.getMonth() + 1) + d.getDate();
 
-    res.json(util.successCode(res, 'success'));
+    res.json(util.successCode(res, tempDate));
 
 });
 
