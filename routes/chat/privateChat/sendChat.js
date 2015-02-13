7a476ef5fefaf686786aae9f47c35e2f590321e1
luume <darkones@naver.com>
2015-02-13 12:19:59 +0900

    d

--- routes/chat/privateChat/sendChat.js
+++ routes/chat/privateChat/sendChat.js
@@ -50,7 +50,7 @@ router.post('/', function(req, res){
         var d = new Date();
         var tempDate = pad2(d.getFullYear().toString()) + pad2((d.getMonth() + 1).toString()) + pad2(d.getDate().toString()
           + pad2(d.getHours().toString()) + pad2(d.getMinutes().toString()));
-
+        console.log('날짜형식', tempDate);
         gcmSetting.gcmSend([messageTO], {messageData : messageData, privateChatRegDate : tempDate });
         console.log('응?');
     })
