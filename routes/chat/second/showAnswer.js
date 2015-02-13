fe862ffc00e873be1076e51a5c913272eb29d2b5
dragojin <dragojin@naver.com>
2015-02-13 17:39:40 +0900

    ddd
    dd

--- routes/chat/second/showAnswer.js
+++ routes/chat/second/showAnswer.js
@@ -9,7 +9,15 @@ router.get('/:chatroomNo', function(req, res){
         res.json({success:0, message:"Error(빈값이 넘어왔습니다.[chatroomNo])", result:null});
         return;
     }
+    /* 질문 가져오기
+    select questionData, questionGuideData from QUESTION
+    where questionNo = (select questionNo from CHATROOM where chatroomNo = 9)
+    */
 
+    /* 텍스트 답변 가져오기
+     SELECT TEXT_ANSWER.memberNo, TEXT_ANSWER.textAnswerData from QUESTION inner join
+     TEXT_ANSWER on QUESTION.questionNo = TEXT_ANSWER.questionNo where TEXT_ANSWER.chatroomNo = 9
+     */
 
     var m = util.createValueObject('Member');
     var c = util.createValueObject('ChatRoom');
