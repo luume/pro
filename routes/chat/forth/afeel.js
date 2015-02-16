996f98a996270910e4f89c2ad2770107fde5c8a0
dragojin <dragojin@naver.com>
2015-02-16 17:55:49 +0900

    sss
    sss

--- routes/chat/forth/afeel.js
+++ routes/chat/forth/afeel.js
@@ -17,15 +17,15 @@ router.post('/:memberTo', function(req, res){
     var memberNo = req.session.memberNo; //자기 자신
     var memberTo = req.params.memberTo; //어필할 상대
     var memberGender = req.body.memberGender; //현재 사용자 성별
-    //var rank = req.body.rank == undefined ? -1: req.body.rank;
-    //console.log('rank',rank);
-    //var omegi = 0;
-    //if(rank == 0 || rank == 1){
-    //    omegi = 5;
-    //}else if(rank == 2){
-    //    omegi = 10;
-    //}
-    var omegi = 5; //어필할 상대
+    var rank = req.body.rank == undefined ? -1: req.body.rank;
+    console.log('rank',rank);
+    var omegi = 0;
+    if(rank == 0 || rank == 1){
+        omegi = 5;
+    }else if(rank == 2){
+        omegi = 10;
+    }
+    //var omegi = req.body.omegi; //어필할 상대
     var chatroomNo = req.body.chatroomNo;
 
     //if(memberNo == "" || memberNo == undefined){
