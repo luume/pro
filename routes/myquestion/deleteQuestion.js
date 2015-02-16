e38c29493f99b8b1643b8c1714d036273a0bdab8
dragojin <dragojin@naver.com>
2015-02-16 14:58:32 +0900

    ddd
    ddd

--- routes/myquestion/deleteQuestion.js
+++ routes/myquestion/deleteQuestion.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var util = require('../../afeel/util/vo');
 var afeelQuery = require('../../afeel/util/afeelQuery');
+var async = require('async');
 router.post('/', function(req, res){
 
     var errobj = {};
@@ -33,6 +34,70 @@ router.post('/', function(req, res){
             }
         });
     }
+
+    async.waterfall([
+            function(callback) {
+                var questionNo = req.body.questionNo;
+                var datas = [];
+                datas.push(questionNo);
+                var queryidname = 'myquestionDeleteSet';
+                afeelQuery.afeelQuery(datas, queryidname , 'myquestion', function (err, datas) {
+                    if (err) {
+                        res.json(err);
+                    }
+                    if (datas.affectedRows == 1) {
+                        //res.json(util.successCode(res, 'success'));
+                        callback(null, 'success');
+
+                    }else {
+                        res.json({success: 0, result: {message: '삭제에 실패하였습니다.(잘못된 질문번호 입력)'}});
+                    }
+                    //console.log('첫번째 처리 성공' , datas[0]);
+
+                })
+            },
+            function(aa, callback) {
+                //console.log('answerData', answerData);
+                //   console.log('여자다');
+                var questionNo = req.body.questionNo;
+                var datas = [];
+                datas.push(questionNo);
+                var queryidname = 'myquestionDelete';
+                afeelQuery.afeelQuery(datas, queryidname , 'myquestion', function (err, datas) {
+                    if (err) {
+                        res.json(err);
+                    }
+                    if (datas.affectedRows == 1) {
+                        //res.json(util.successCode(res, 'success'));
+                        callback(null, 'success');
+
+                    }else {
+                        res.json({success: 0, result: {message: '삭제에 실패하였습니다.(잘못된 질문번호 입력)'}});
+                    }
+                    //console.log('첫번째 처리 성공' , datas[0]);
+
+                })
+
+            }
+        ],	function(err, results) {
+            // console.log('최종 처리');
+            // console.log('results' , results); // result <- done
+            //var arr = [];
+            //arr.push(datas);
+            //res.json({
+            //    success : 1,
+            //    message : 'OK',
+            //    result : {
+            //        questionData: questionData.questionData,
+            //        questionGuideData: questionData.questionGuideData,
+            //        textAnswer : datas
+            //    }
+            //});
+            res.json(util.successCode(res, results));
+            //res.json({success:1, message:'ok', result:datas[0]});
+            //res.json(util.successCode(res, results));
+        }
+    );
 
 
 });
