476acb53a0969b4dd44da3cc9f99324f89f56a71
dragojin <dragojin@naver.com>
2015-02-16 14:59:05 +0900

    ddd
    dd

--- routes/myquestion/deleteQuestion.js
+++ routes/myquestion/deleteQuestion.js
@@ -6,34 +6,34 @@ var afeelQuery = require('../../afeel/util/afeelQuery');
 var async = require('async');
 router.post('/', function(req, res){
 
-    var errobj = {};
-    errobj = util.variableCheck(req.body, 1);
-    if(errobj != undefined ){
-        console.log('if',errobj);
-        res.json(errobj);
-        //return;
-    }else{
-        console.log('else',errobj);
-        var questionNo = req.body.questionNo;
-        var datas = [];
-        datas.push(questionNo);
-        global.queryName = 'myquestion';
-        var queryidname = 'myquestionDelete';
-        afeelQuery.afeelQuery(datas, queryidname , 'myquestion', function (err, datas) {
-
-            if (err) {
-                res.json(err);
-            }
-            if (datas.affectedRows == 1) {
-                res.json(util.successCode(res, 'success'));
-
-
-            }else {
-                res.json({success: 0, result: {message: '삭제에 실패하였습니다.(잘못된 질문번호 입력)'}});
-
-            }
-        });
-    }
+    //var errobj = {};
+    //errobj = util.variableCheck(req.body, 1);
+    //if(errobj != undefined ){
+    //    console.log('if',errobj);
+    //    res.json(errobj);
+    //    //return;
+    //}else{
+    //    console.log('else',errobj);
+    //    var questionNo = req.body.questionNo;
+    //    var datas = [];
+    //    datas.push(questionNo);
+    //    global.queryName = 'myquestion';
+    //    var queryidname = 'myquestionDelete';
+    //    afeelQuery.afeelQuery(datas, queryidname , 'myquestion', function (err, datas) {
+    //
+    //        if (err) {
+    //            res.json(err);
+    //        }
+    //        if (datas.affectedRows == 1) {
+    //            res.json(util.successCode(res, 'success'));
+    //
+    //
+    //        }else {
+    //            res.json({success: 0, result: {message: '삭제에 실패하였습니다.(잘못된 질문번호 입력)'}});
+    //
+    //        }
+    //    });
+    //}
 
     async.waterfall([
             function(callback) {
