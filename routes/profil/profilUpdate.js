a1d0118353aa03146be34f39c0913301fa907701
luume <darkones@naver.com>
2015-02-17 16:24:43 +0900

    s

--- routes/profil/profilUpdate.js
+++ routes/profil/profilUpdate.js
@@ -188,9 +188,9 @@ router.post('/', function(req, res){
                             }else {
                                 var arr = [];
                                 arr.push(req.session.memberNo);
-                                arr.push( 'http://54.92.4.84:3000/mmm/' + item.split('/')[4]);
-                                arr.push( 'http://54.92.4.84:3000/mmm/' + item.split('/')[4]);
-                                arr.push( 'http://54.92.4.84:3000/mmm/' + item.split('/')[4]);
+                                arr.push( 'http://54.92.4.84:3000/mmm/' + item.name);
+                                arr.push( 'http://54.92.4.84:3000/mmm/' + item.name);
+                                arr.push( 'http://54.92.4.84:3000/mmm/' + item.name);
                                 if (ii == 0) {
                                     afeelQuery.afeelQuery(arr, 'insertProfilMain', 'profil', function (err, datas) {
                            /*             if (err) {
