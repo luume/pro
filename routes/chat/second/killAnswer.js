73ee0ad7df20dfbe0c760f1246a462ff85a90c3c
dragojin <dragojin@naver.com>
2015-02-18 15:23:49 +0900

    dddd
    dd

--- routes/chat/second/killAnswer.js
+++ routes/chat/second/killAnswer.js
@@ -2,16 +2,34 @@ var express = require('express');
 var router = express.Router();
 
 var util = require('../../../afeel/util/vo');
+var afeelQuery = require('../../../afeel/util/afeelQuery');
 
 router.post('/', function(req, res){
     var chatroomNo = req.body.chatroomNo;
-    if(chatroomNo == "" || chatroomNo == undefined){
-        res.json({success:0, message:"Error(빈값이 넘어왔습니다.[chatroomNo])", result:null});
-        return;
-    }
+    var memberNo = req.body.memberNo;
+    var rank = 3;
 
+    var datas = [];
+    datas.push(memberNo);
+    datas.push(chatroomNo);
+    datas.push(rank);
 
-    res.json(util.successCode(res, 'success'));
+    var queryidname = 'killMan';
+    afeelQuery.afeelQuery(datas, queryidname , 'chat', function (err, datas) {
+        if (err) {
+            res.json(err);
+            return;
+        }
+        if (datas.affectedRows == 1) {
+            res.json(util.successCode(res, 'success'));
+        }else {
+            res.json({success: 0, result: {message: '3등 탈락자 선정에 실패'}});
+            return;
+        }
+    });
+
+    //
+    //res.json(util.successCode(res, 'success'));
 
 });
 
