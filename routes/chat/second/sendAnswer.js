b0f3901948274d38fb705a5f3fc953e2124c7b04
dragojin <dragojin@naver.com>
2015-02-18 14:06:22 +0900

    ddd
    dd

--- routes/chat/second/sendAnswer.js
+++ routes/chat/second/sendAnswer.js
@@ -4,19 +4,41 @@ var router = express.Router();
 var util = require('../../../afeel/util/vo');
 
 router.post('/', function(req, res){
-    var chatroomNo = req.body.chatroomNo
-    if(chatroomNo == "" || chatroomNo == undefined){
-        res.json({success:0, message:"Error(빈값이 넘어왔습니다.[chatroomNo])", result:null});
-        return;
-    }
+    var chatroomNo = req.body.chatroomNo;
+    var memberNo = req.session.memberNo;
+    //if(chatroomNo == "" || chatroomNo == undefined){
+    //    res.json({success:0, message:"Error(빈값이 넘어왔습니다.[chatroomNo])", result:null});
+    //    return;
+    //}
 
-    var textAnwerData = req.body.textAnwerData;
-    if(textAnwerData == "" || textAnwerData == undefined){
-        res.json({success:0, message:"Error(빈값이 넘어왔습니다.[textAnwerData])", result:null});
-        return;
-    }
+    var textAnswerData = req.body.textAnswerData;
+    var questionNo = req.body.questionNo;
 
-    res.json(util.successCode(res, 'success'));
+    var datas = [];
+    datas.push(textAnswerData);
+    datas.push(chatroomNo);
+    datas.push(memberNo);
+    datas.push(questionNo);
+
+    var queryidname = 'sendTextAnswer';
+    afeelQuery.afeelQuery(datas, queryidname , 'chat', function (err, datas) {
+        if (err) {
+            res.json(err);
+            return;
+        }
+        if (datas.affectedRows == 1) {
+            res.json(util.successCode(res, 'success'));
+        }else {
+            res.json({success: 0, result: {message: '답변 전송에 실패'}});
+            return;
+        }
+    });
+    //if(textAnwerData == "" || textAnwerData == undefined){
+    //    res.json({success:0, message:"Error(빈값이 넘어왔습니다.[textAnwerData])", result:null});
+    //    return;
+    //}
+
+    //res.json(util.successCode(res, 'success'));
 
 });
 
