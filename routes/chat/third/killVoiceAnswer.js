d828ba5a9f843004ecd9806a28debc9d765cf13a
dragojin <dragojin@naver.com>
2015-02-18 15:57:48 +0900

    dd
    dddd

--- routes/chat/third/killVoiceAnswer.js
+++ routes/chat/third/killVoiceAnswer.js
@@ -2,16 +2,50 @@ var express = require('express');
 var router = express.Router();
 
 var util = require('../../../afeel/util/vo');
+var afeelQuery = require('../../../afeel/util/afeelQuery');
 
 router.post('/', function(req, res){
     var chatroomNo = req.body.chatroomNo;
-    if(chatroomNo == "" || chatroomNo == undefined){
-        res.json({success:0, message:"Error(빈값이 넘어왔습니다.[chatroomNo])", result:null});
-        return;
-    }
+    var memberNo = req.body.memberNo; //버릴 회원 번호
+    var winMemberNo = req.body.winMemberNo; //이긴 회원 번호
+    var rank = 2;
 
-    res.json(util.successCode(res, 'success'));
+    var datas = [];
+    datas.push(memberNo);
+    datas.push(chatroomNo);
+    datas.push(rank);
 
+    var queryidname = 'killMan';
+    afeelQuery.afeelQuery(datas, queryidname , 'chat', function (err, datas) {
+        if (err) {
+            res.json(err);
+            return;
+        }
+        if (datas.affectedRows == 1) {
+
+            rank = 1;
+            var windatas = [];
+            windatas.push(winMemberNo);
+            windatas.push(chatroomNo);
+            windatas.push(rank);
+
+            afeelQuery.afeelQuery(windatas, queryidname , 'chat', function (err, datas) {
+                if (err) {
+                    res.json(err);
+                    return;
+                }
+                if (datas.affectedRows == 1) {
+                    res.json(util.successCode(res, 'success'));
+                }else {
+                    res.json({success: 0, result: {message: '1등 선정에 실패'}});
+                    return;
+                }
+            });
+        }else {
+            res.json({success: 0, result: {message: '2등 선정에 실패'}});
+            return;
+        }
+    });
 });
 
 module.exports = router;
