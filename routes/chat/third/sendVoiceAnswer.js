d89a82cd0a37de5024d7706734c92a4b46c7c15c
luume <darkones@naver.com>
2015-02-21 15:35:21 +0900

    sf

--- routes/chat/third/sendVoiceAnswer.js
+++ routes/chat/third/sendVoiceAnswer.js
@@ -27,7 +27,7 @@ router.post('/', function(req, res){
     var questionNo = req.body.questionNo;
 
     var datas = [];
-    datas.push(voiceAnswerData.name);
+    datas.push(voiceAnswerData[0].name.split('.')[0] + voiceAnswerData[0].name.split('.')[1]);
     datas.push(chatroomNo);
     datas.push(memberNo);
     datas.push(questionNo);
