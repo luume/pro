bb1648fab745cba95af6ba6b8a580431e10fdb3d
luume <darkones@naver.com>
2015-02-22 16:49:59 +0900

    d
    d

--- routes/chat/third/sendVoiceAnswer.js
+++ routes/chat/third/sendVoiceAnswer.js
@@ -15,7 +15,7 @@ var multer  = require('multer');
 router.use(multer({
     dest: './public/rec/',
     rename: function (fieldname, filename) {
-        return filename + Date.now() + '.jpg'
+        return filename + Date.now() + '.mp4'
     }
 }));
 
